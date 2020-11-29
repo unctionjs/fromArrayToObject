e6a554907e32f34be53756fad10f9ea6b7a93040
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-28 18:50:20 -0800

    Denormalizing some types to make things a little bit easier

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import reduceValues from "@unction/reducevalues";
 import attach from "@unction/attach";
-import {ObjectType} from "./types";
 
 export default function fromArrayToObject<A> (array: Array<[string | number, A]>) {
   return reduceValues(
